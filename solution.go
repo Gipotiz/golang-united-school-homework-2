01aea390e2dc8054bf328d7e378c057d0c117e73
mdalbrid <k1_2-d@mail.ru>
2022-07-02 23:45:12 +0300

    add const

--- solution.go
+++ solution.go
@@ -10,6 +10,12 @@ import "math"
 // CalcSquare(10.0, SidesSquare)
 // CalcSquare(10.0, SidesCircle)
 
+const (
+	SidesTriangle = 3
+	SidesSquare   = 4
+	SidesCircle   = 0
+)
+
 func CalcSquare(sideLen float64, sidesNum int32) float64 {
 	if sidesNum == 3 {
 		p := sideLen * 3 / 2
